57a55f12f800c462984b2cddd9e53f21e220f8f8
lloydzhou <lloydzhou@qq.com>
2023-02-03 18:26:32 +0800

    add readme

--- src/history.ts
+++ src/history.ts
@@ -2,20 +2,16 @@
 
 export default class HistoryManager {
   protected actions = []
-  protected current = 0
+  protected current = -1
 
   // 使用一个列表加上一个指针来处理
   // 列表中存放{commit, rollback}两个函数。undo的时候调用rollback，redo的时候调用commit
   // 添加操作的时候，使用splice将redo的列表删除，同时在后面增加当前的操作
-  constructor() {
-    this.actions = []
-    this.current = -1
-  }
   push(commit, rollback) {
     return Promise.resolve().then(() => {
       return commit()
     }).then(res => {
-      console.log('push res', res, 'current', this.current, 'redoSize', this.redoSize, 'length', this.length)
+      // console.log('push res', res, 'current', this.current, 'redoSize', this.redoSize, 'length', this.length)
       this.actions.splice(++this.current, this.redoSize + 1, { commit, rollback })
     })
   }
@@ -38,14 +34,14 @@ export default class HistoryManager {
     return Promise.resolve(this.canRedo).then(canRedo => {
       if (canRedo) {
         const { commit } = this.actions[this.current + 1]
-        console.log('commit', this.current, commit)
+        // console.log('commit', this.current, commit)
         if (typeof commit == "function") {
           return commit()
         }
       }
       throw new Error()
     }).then(res => {
-      console.log('redo res', res)
+      // console.log('redo res', res)
       this.current += 1
     })
   }
@@ -53,14 +49,14 @@ export default class HistoryManager {
     return Promise.resolve(this.canUndo).then(canUndo => {
       if (canUndo) {
         const { rollback } = this.actions[this.current]
-        console.log('rollback', this.current, rollback)
+        // console.log('rollback', this.current, rollback)
         if (typeof rollback == "function") {
           return rollback()
         }
       }
       throw new Error()
     }).then(res => {
-      console.log('undo res', res)
+      // console.log('undo res', res)
       this.current -= 1
     })
   }
